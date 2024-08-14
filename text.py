069776ca989963b2160e125b9a9f5ee3b88d60bd
Zoe <z.camus@alumno.um.edu.ar>
2024-08-14 18:54:30 -0300

    arreglo de tests

--- text.py
+++ text.py
@@ -1,11 +1,15 @@
 import unittest
-
 from main import suma
 
-class TestSuma(unittest.TestCase):
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(suma(1,2), 3)
 
-    def test_suma(self):
-        self.assertEqual(suma(1, 2), 3)
+    def test_sum2(self):
+        self.assertEqual(suma(3,3), 6)
+
+    def test_sum3(self):
+        self.assertEqual(suma(6,3), 9)
 
 if __name__ == '__main__':
     unittest.main()
