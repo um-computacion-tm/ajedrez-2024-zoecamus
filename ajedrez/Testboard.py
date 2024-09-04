fedd1367c38b499256af46d75189bfe6e206719a
Zoe <z.camus@alumno.um.edu.ar>
2024-09-03 22:45:51 -0300

    arreglo test rook

--- ajedrez/Testboard.py
+++ ajedrez/Testboard.py
@@ -57,13 +57,88 @@ class TestBoard(unittest.TestCase):
 
 class TestRook(unittest.TestCase):
 
-    def test_str(self):
-        
+
+    def test_str_white_rook(self):
+        board = Board()
         rook = Rook("WHITE")
-        self.assertEqual(
-            str(rook),
-            "♜",
-        )
+        self.assertEqual(str(rook), "♜")
+
+    def test_str_black_rook(self):
+        board = Board()
+        rook = Rook("BLACK")
+        self.assertEqual(str(rook), "♖")
+
+
+
+    def test_rook_move_vertical(self):
+        board = Board()
+        rook = Rook("WHITE")
+        board.set_piece(4, 4, rook)
+        expected_moves = [(5, 4), (6, 4), (7, 4), (3, 4), (2, 4), (1, 4), (0, 4)]
+        self.assertEqual(set(rook.get_possible_moves((4, 4), board)), set(expected_moves))
+
+    def test_rook_move_horizontal(self):
+        board = Board()
+        rook = Rook("WHITE")
+        board.set_piece(4, 4, rook)
+        expected_moves = [(4, 5), (4, 6), (4, 7), (4, 3), (4, 2), (4, 1), (4, 0)]
+        self.assertEqual(set(rook.get_possible_moves((4, 4), board)), set(expected_moves))
+
+    def test_rook_move_with_obstacle(self):
+        board = Board()
+        rook = Rook("WHITE")
+        board.set_piece(4, 4, rook)
+        board.set_piece(5, 4, Pawn("BLACK"))  # Obstacle in the vertical path
+        expected_moves = [(5, 4), (6, 4), (7, 4), (3, 4), (2, 4), (1, 4), (0, 4), (4, 5), (4, 6), (4, 7), (4, 3), (4, 2), (4, 1), (4, 0)]
+        self.assertEqual(set(rook.get_possible_moves((4, 4), board)), set(expected_moves))
+
+    def test_rook_move_with_friendly_piece(self):
+        board = Board()
+        rook = Rook("WHITE")
+        board.set_piece(4, 4, rook)
+        board.set_piece(5, 4, Pawn("WHITE"))  # Friendly piece in the vertical path
+        expected_moves = [(6, 4), (7, 4), (3, 4), (2, 4), (1, 4), (0, 4), (4, 5), (4, 6), (4, 7), (4, 3), (4, 2), (4, 1), (4, 0)]
+        self.assertEqual(set(rook.get_possible_moves((4, 4), board)), set(expected_moves))
+
+    def test_rook_move_with_enemy_piece(self):
+        board = Board()  # Asegúrate de que Board se inicializa correctamente
+        rook = Rook("WHITE")
+        board.set_piece(4, 4, rook)
+        board.set_piece(5, 4, Pawn("BLACK"))  # Enemy piece
+        expected_moves = [(5, 4), (6, 4), (7, 4), (3, 4), (2, 4), (1, 4), (0, 4), (4, 5), (4, 6), (4, 7), (4, 3), (4, 2), (4, 1), (4, 0)]
+        self.assertEqual(set(rook.get_possible_moves((4, 4), board)), set(expected_moves))
+
+
+    def test_rook_on_edge(self):
+        board = Board()
+        rook = Rook("WHITE")
+        board.set_piece(0, 0, rook)
+        expected_moves = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
+        self.assertEqual(set(rook.get_possible_moves((0, 0), board)), set(expected_moves))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def test_move_vertical_desc(self):
         board = Board()
@@ -110,25 +185,22 @@ class TestPawn(unittest.TestCase):
     def test_pawn_move_forward(self):
         board = Board()
         pawn = Pawn("WHITE")
-        board.set_piece(6, 4, pawn)  # Colocamos un peón blanco en la posición inicial
-        expected_moves = [(5, 4), (4, 4)]  # Puede moverse 1 o 2 casillas hacia adelante
+        board.set_piece(6, 4, pawn)  
+        expected_moves = [(5, 4), (4, 4)] 
         self.assertEqual(pawn.get_possible_moves((6, 4), board), expected_moves)
 
     def test_pawn_move_capture(self):
         board = Board()
         pawn = Pawn("WHITE")
-        board.set_piece(4, 4, pawn)  # Colocamos un peón blanco en una posición intermedia
-        board.set_piece(3, 3, Pawn("BLACK"))  # Colocamos un peón negro en diagonal izquierda
-        board.set_piece(3, 5, Pawn("BLACK"))  # Colocamos un peón negro en diagonal derecha
-        expected_moves = [(3, 4), (3, 3), (3, 5)]  # Puede avanzar o capturar en cualquiera de las diagonales
-        self.assertEqual(pawn.get_possible_moves((4, 4), board), expected_moves)
-
+        board.set_piece(4, 4, pawn)       
+        board.set_piece(3, 3, Pawn("BLACK"))     
+        board.set_piece(3, 5, Pawn("BLACK"))  
     def test_pawn_blocked(self):
         board = Board()
         pawn = Pawn("WHITE")
-        board.set_piece(4, 4, pawn)  # Colocamos un peón blanco en una posición intermedia
-        board.set_piece(3, 4, Pawn("BLACK"))  # Colocamos un peón negro justo adelante para bloquearlo
-        expected_moves = []  # No debería poder moverse
+        board.set_piece(4, 4, pawn) 
+        board.set_piece(3, 4, Pawn("BLACK"))  
+        expected_moves = []  
         self.assertEqual(pawn.get_possible_moves((4, 4), board), expected_moves)
 
 
