8a5fcbe057517d249cd1eef479fb5c27d81fd49c
Zoe <z.camus@alumno.um.edu.ar>
2024-09-09 22:41:21 -0300

    movimientos bishop

--- ajedrez/TestBishop.py
+++ ajedrez/TestBishop.py
@@ -1,6 +1,6 @@
 import unittest
 from classboard import Board
-from pieces import Bishop
+from movebishop import Bishop
 
 class TestBishop(unittest.TestCase):
     
@@ -14,6 +14,17 @@ class TestBishop(unittest.TestCase):
 
         expected_moves = [(2, 2), (1, 1), (0, 0)]
         self.assertEqual(possible_moves, expected_moves)
+
+    def test_move_diagonal_up_right(self):
+        bishop = Bishop('WHITE', self.board)
+        self.board.positions[3][3] = bishop
+        possible_moves = bishop.get_possible_moves((3, 3))
+
+        expected_moves = [(2, 2), (1, 1), (0, 0), (2, 4), (1, 5), (0, 6)]
+        self.assertEqual(possible_moves, expected_moves)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
