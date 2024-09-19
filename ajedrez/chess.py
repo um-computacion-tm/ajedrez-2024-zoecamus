e2e7275ceac897ad0ce9bc5c400fe6ec7fe1f60f
Zoe <z.camus@alumno.um.edu.ar>
2024-09-18 22:40:16 -0300

    cambios en archivo chees.py

--- ajedrez/chess.py
+++ ajedrez/chess.py
@@ -5,34 +5,22 @@ def main():
      while chess.is_playing():
          play(chess)
 
-def play(Chess):
-     try:
-         print(Chess.show_board())
-         print('Turn: ' + Chess.turn)
-         from_row =  int(input('From row: '))
-         from_col =  int(input('From column: '))
-         to_row =  int(input('To row: '))
-         to_col =  int(input('To column: '))
-
-         Chess.move(from_row, from_col, to_row, to_col)
-
-     except Exception as e:
-         print('Invalid error')
-
-                
-@property
-def turn(self):
-        return self.__turn__
-
-def show_board(self):
-        return str(self.__board__)
-
-def change_turn(self):
-        if self.__turn__ == "WHITE":
-            self.__turn__ = "BLACK"
-        else:
-             self.__turn__ = "WHITE"
+def play(chess_instance):
+    try:
+        print(chess_instance.show_board())
+        print('Turn: ' + chess_instance.turn)
+        
+        from_row = int(input('From row: '))
+        from_col = int(input('From column: '))
+        to_row = int(input('To row: '))
+        to_col = int(input('To column: '))
+        
+        chess_instance.move(from_row, from_col, to_row, to_col)
     
-    
+        chess_instance.change_turn()
+
+    except Exception as e:
+        print(f'Invalid move: {e}')
+
 if __name__ == '__main__':
     main()
